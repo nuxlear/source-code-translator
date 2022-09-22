4b578a11d3007e0312b21fd9731ddeb8d93756a3
Jun-Won Hwang <nuclear1221@gmail.com>
2022-09-23 01:48:26 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import gradio as gr
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# response = openai.Completion.create(
+#   model="code-davinci-002",
+#   prompt="# Python 3 \ndef remove_common_prefix(x, prefix, ws_prefix): \n    x[\"completion\"] = x[\"completion\"].str[len(prefix) :] \n    if ws_prefix: \n        # keep the single whitespace as prefix \n        x[\"completion\"] = \" \" + x[\"completion\"] \nreturn x \n\n# Explanation of what the code does\n\n#",
+#   temperature=0,
+#   max_tokens=64,
+#   top_p=1.0,
+#   frequency_penalty=0.0,
+#   presence_penalty=0.0
+# )
+
+
+def generate_explanation(input_text, mode=None):
+    if mode == 'generate':
+        input_text = f'"""\n{input_text}\n"""'
+    if mode == 'explain':
+        input_text = f'# Python 3\n{input_text}\n\n# Explanation of what the code does\n\n#'
+
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=input_text,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    answer = response.choices[0]
+    return answer.text
+
+
+if __name__ == '__main__':
+#     code = '''name = input("What is your name? ")
+# print("Hello, " + name)'''
+#     generate_explanation(code)
+    demo = gr.Interface(fn=generate_explanation,
+                        inputs=[
+                            gr.Textbox(lines=40, max_lines=40),
+                            gr.Radio(choices=['generate', 'explain'],
+                                     label=['Generate Code', 'Explain Code']),
+                        ],
+                        outputs=[
+                            gr.Textbox(lines=50, max_lines=50),
+                        ])
+    demo.launch()
